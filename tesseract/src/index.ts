9c3bdce774e0eaa86525335ae5eacffd0c9bea35
Swanav <swanav@outlook.com>
2018-06-28 13:39:28 +0530

    Tesseract module created.

--- tesseract/src/index.ts
+++ tesseract/src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello, world!');
+import { connect } from 'mqtt'
+
+const client = connect('mqtt://localhost:61616');
+
+// client.on('connect', () => {
+//     console.log('Connected')
+//     client.subscribe('foo.bar')
+//     console.log('Subscribed')
+// })
+
+// client.on('message', (topic, message) => {
+//     console.log(`Received: `, topic, message)
+// })
